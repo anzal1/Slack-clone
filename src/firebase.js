51731dcc39b46e14242ea32ffcd272f6d70adc2e
anzal1 <you@example.com>
2022-03-20 02:23:55 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBEy-T__sbxJ7l9GF3vYSMrVzFLsooeRSI",
-  authDomain: "slack-clone-30c59.firebaseapp.com",
-  projectId: "slack-clone-30c59",
-  storageBucket: "slack-clone-30c59.appspot.com",
-  messagingSenderId: "1033596060845",
-  appId: "1:1033596060845:web:a2ce86efad4a79f6037224",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
